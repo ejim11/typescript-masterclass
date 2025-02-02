5a4338f87c1ac68a67e5d62546ac7a183840acb0
ejim11 <favourejim56@gmail.com>
2025-02-02 08:12:19 +0100

    ✅ intro

---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict";
+const students = [
+    {
+        name: "John",
+        score: 17,
+    },
+    {
+        name: "Mark",
+        score: 28,
+    },
+];
+for (const stud of students) {
+    //   console.log("Name: ", stud.name);
+    //   console.log("Score: ", stud.score);
+}
+//////////////////////////////////////
+function addNumbers(a, b) {
+    return a + b;
+}
+// console.log(addNumbers(2, 3));
+// console.log(addNumbers(3932, 3));
+/////////////////////////////////////////
