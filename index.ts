23d0592a16d014bc8ba98eb2bd9377c01f87a45c
ejim11 <favourejim56@gmail.com>
2025-02-02 08:12:29 +0100

    ✅ intro

---
+++ index.ts
@@ -0,0 +1,26 @@
+const students = [
+  {
+    name: "John",
+    score: 17,
+  },
+  {
+    name: "Mark",
+    score: 28,
+  },
+];
+
+for (const stud of students) {
+  //   console.log("Name: ", stud.name);
+  //   console.log("Score: ", stud.score);
+}
+
+//////////////////////////////////////
+
+function addNumbers(a: number, b: number): number {
+  return a + b;
+}
+
+// console.log(addNumbers(2, 3));
+// console.log(addNumbers(3932, 3));
+
+/////////////////////////////////////////
